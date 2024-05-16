dbf41cd48315f18446a2a17c2d1e45d734b63d97
Luca Mehl <lsm20@ic.ac.uk>
2024-05-16 02:28:47 +0100

    fixed synch

--- pcoNode7.py
+++ pcoNode7.py
@@ -1,3 +1,4 @@
+import math
 import random
 import matplotlib.pyplot as plt
 
@@ -5,6 +6,7 @@ import networkx as nx
 import simpy
 import statistics
 import numpy as np
+from pprint import pprint as pp
 
 
 class PCONode7:
@@ -43,7 +45,7 @@ class PCONode7:
         """Main loop for the node"""
 
         # sleep random time before starting
-        yield self.env.timeout(self.state['initial_time_offset']*OVERALL_MULT)
+        yield self.env.timeout(self.state['initial_time_offset'] * OVERALL_MULT)
 
         while True:
 
@@ -59,7 +61,7 @@ class PCONode7:
                     self.highest_msg_this_epoch = new_msg
 
                     # synchronize to it
-                    self.next_period = DEFAULT_PERIOD_LENGTH - (self.period - self.timer) #- 1*OVERALL_MULT
+                    self.next_period = DEFAULT_PERIOD_LENGTH - (self.period - self.timer)  # - 1*OVERALL_MULT
                     # next period should be the time since the todo
                     self.log('synchronized to node', new_msg[0], 'which has epoch', new_msg[1],
                              'setting next period to', self.next_period)
@@ -69,10 +71,10 @@ class PCONode7:
                     # self.log('ignoring message from node', new_msg[0], 'with epoch', new_msg[1])
                     pass
 
-                if new_msg[1] >= self.epoch + 2*OVERALL_MULT:
+                if new_msg[1] >= self.epoch + 2:
                     # we're super out of sync, match their epoch number
                     self.log("super out of sync, matching epoch number")
-                    self.epoch = new_msg[1]
+                    self.epoch = new_msg[1] - 1
 
             # timer expired
             if self.timer >= self.period:
@@ -108,10 +110,13 @@ class PCONode7:
 
             # update local timer (can be made stochastic)
             # tick_len = int(rng.normal(1 + self.clock_drift_rate * 1e-6, 0.001) * OVERALL_MULT)  # self.clock_drift_scale
-            tick_len = int(rng.normal(1 * RECEPTION_LOOP_TICKS + self.clock_drift_rate * 3e-6 * RECEPTION_LOOP_TICKS, self.clock_drift_scale * RECEPTION_LOOP_TICKS)) # self.clock_drift_scale
+            tick_len = int(rng.normal(1 * RECEPTION_LOOP_TICKS + self.clock_drift_rate * 3e-6 * RECEPTION_LOOP_TICKS,
+                                      self.clock_drift_scale * RECEPTION_LOOP_TICKS))  # self.clock_drift_scale
             # print(tick_len)
             # self.timer += tick_len
-            self.timer += RECEPTION_LOOP_TICKS #* OVERALL_MULT // 2
+            self.timer += RECEPTION_LOOP_TICKS  # * OVERALL_MULT // 2
+
+            self.log_phase_helper()
 
             # yield self.env.timeout(1)
             yield self.env.timeout(tick_len)
@@ -119,48 +124,67 @@ class PCONode7:
     def _tx(self, message):
         """Broadcast a *message* to all receivers."""
         self.log_fire()
-        # self.log('fired')
         if not self.neighbors:
             raise RuntimeError('There are no neighbors to send to.')
 
         for neighbor in self.neighbors:
-            # distance = self.distance_euc(self.pos, neighbor.pos) / M_TO_PX
+            distance = distance_euc(self.pos, neighbor.pos)
             # reception_prob = 1 / (1 + distance ** 2)
-            # print(reception_prob)
+            # reception_prob = 1 * math.e ** (-(distance ** 2))
+            reception_prob = reception_probability(distance)
+            # print('from', self.id, 'to', neighbor.id, ':', reception_prob)
 
             # message reception probability proportional to inverse distance squared
-            if random.random() < NEIGHBOR_RECEPTION_PROB:  # reception_prob:
+            # if random.random() < NEIGHBOR_RECEPTION_PROB:
+            if random.random() < reception_prob:
                 neighbor.buffer.append(message)
 
-                # log reception
-                reception_x.append(self.env.now)
-                reception_y.append(neighbor.id)
-
-    def distance_euc(self, a, b):
-        return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
+                self.log_reception(neighbor)
 
     def log(self, *message):
         """Log a message with the current time and node id."""
-        print('node', self.id, '|', 'time', self.env.now / OVERALL_MULT, '| epoch', self.epoch, '|', *message)
+        if LOGGING:
+            print('node', self.id, '|', 'time', self.env.now / OVERALL_MULT, '| epoch', self.epoch, '|', *message)
 
     def log_phase(self):
         node_phase_x[self.id].append(self.env.now)
         node_phase_y[self.id].append(self.timer)
+        node_phase_percentage_x[self.id].append(self.env.now)
+        node_phase_percentage_y[self.id].append((self.timer / max(self.period, 1)) * 100)
+
+    def log_phase_helper(self):
+        """needed to instantly set next env tick phase to 0, otherwise waits until next large tick to set to zero,
+        messing up the interpolation when graphing"""
+        if self.timer >= self.period:
+            node_phase_x[self.id].append(self.env.now + 1)
+            node_phase_y[self.id].append(0)
+            node_phase_percentage_x[self.id].append(self.env.now + 1)
+            node_phase_percentage_y[self.id].append(0)
 
     def log_fire(self):
-        # node_fires[self.id][self.env.now] = 5
         fire_x.append(self.env.now)
         fire_y.append(self.id)
 
     def log_suppress(self):
-        # node_suppresses[self.id][self.env.now] = 1
         suppress_x.append(self.env.now)
         suppress_y.append(self.id)
 
     def log_epoch(self):
-        # node_epochs[self.id][self.env.now] = self.epoch
         node_epochs_x[self.id].append(self.env.now)
         node_epochs_y[self.id].append(self.epoch)
+
+    def log_reception(self, neighbor):
+        reception_x.append(self.env.now)
+        reception_y.append(neighbor.id)
+
+
+def distance_euc(a, b):
+    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5 / M_TO_PX
+
+
+def reception_probability(distance):
+    return 1 / (1 + distance ** DISTANCE_EXPONENT)
+
 
 env = simpy.Environment()
 
@@ -190,11 +214,25 @@ TOPO_MINI_FC = {
     2: [0, 1],
 }
 
-##########
+TOPO_FOUR_BRIDGES = {
+    0: [1, 2],
+    1: [0, 2, 3],
+    2: [0, 1, 10],
 
+    3: [4, 5, 1],
+    4: [3, 5],
+    5: [3, 4, 6],
+
+    6: [7, 8, 5],
+    7: [6, 8],
+    8: [6, 7, 9],
+
+    9: [10, 11, 8],
+    10: [9, 11, 2],
+    11: [9, 10],
+}
 
 # TOPO_FULLY_CONNECTED = {}
-
 # add another 50 nodes to the fully connected topo and the init state variable
 # for i in range(50):
 #     TOPO_FULLY_CONNECTED[i] = [j for j in range(50) if j != i]
@@ -203,63 +241,61 @@ TOPO_MINI_FC = {
 #          'clock_drift_scale': 0.1, 'linestyle': 'dashdot', 'hatch': 'x'})
 
 
-topo = TOPO_FULLY_CONNECTED
-# topo = TOPO_BRIDGE
+########## CONSTANTS ##########
+# topo = TOPO_FULLY_CONNECTED
 # topo = TOPO_MINI_FC
+topo = TOPO_BRIDGE
+# topo = TOPO_FOUR_BRIDGES
+
+LOGGING = False
+RANDOM_SEED = 10
 OVERALL_MULT = 1000
 RECEPTION_LOOP_TICKS = OVERALL_MULT / 10
-DEFAULT_PERIOD_LENGTH = 100*OVERALL_MULT#100  # 1000 or 999? since 0 is included
-SIM_TIME = 1500*OVERALL_MULT#1500
-# MS_PROB = 1
-MS_PROB = 0
-# MS_PROB = 1
-RANDOM_SEED = 9
-M_TO_PX = 300
-# M_TO_PX = 750
-NEIGHBOR_RECEPTION_PROB = 1 #0.9
+DEFAULT_PERIOD_LENGTH = 100 * OVERALL_MULT
+SIM_TIME = 2000 * OVERALL_MULT
+
+MS_PROB = 0.8  # 1
+
+M_TO_PX = 100  ########## ACCURATE: 100    DENSE: 150      BUGGY: 120
+DISTANCE_EXPONENT = 10  # ACCURATE: 10     DENSE: 8        BUGGY: 3
+
 CLOCK_DRIFT_RATE_OFFSET_RANGE = 100
 CLOCK_DRIFT_VARIABILITY = 0.05
-
-#######
+########## CONSTANTS ##########
 
 # node configuration
 init_state = [
-    {'id': 0, 'initial_time_offset': 0, 'clock_drift_rate': random.randint(-CLOCK_DRIFT_RATE_OFFSET_RANGE, CLOCK_DRIFT_RATE_OFFSET_RANGE), 'clock_drift_scale': CLOCK_DRIFT_VARIABILITY,
-     'linestyle': 'solid',
-     'hatch': '///'},
-    {'id': 1, 'initial_time_offset': 10, 'clock_drift_rate': random.randint(-CLOCK_DRIFT_RATE_OFFSET_RANGE, CLOCK_DRIFT_RATE_OFFSET_RANGE), 'clock_drift_scale': CLOCK_DRIFT_VARIABILITY,
-     'linestyle': 'dashed',
-     'hatch': '--'},
-    {'id': 2, 'initial_time_offset': 20, 'clock_drift_rate': random.randint(-CLOCK_DRIFT_RATE_OFFSET_RANGE, CLOCK_DRIFT_RATE_OFFSET_RANGE), 'clock_drift_scale': CLOCK_DRIFT_VARIABILITY,
-     'linestyle': 'dotted',
-     'hatch': '.'},
-    {'id': 3, 'initial_time_offset': 30, 'clock_drift_rate': random.randint(-CLOCK_DRIFT_RATE_OFFSET_RANGE, CLOCK_DRIFT_RATE_OFFSET_RANGE), 'clock_drift_scale': CLOCK_DRIFT_VARIABILITY,
-     'linestyle': 'dashdot',
-     'hatch': 'x'},
-    {'id': 4, 'initial_time_offset': 40, 'clock_drift_rate': random.randint(-CLOCK_DRIFT_RATE_OFFSET_RANGE, CLOCK_DRIFT_RATE_OFFSET_RANGE), 'clock_drift_scale': CLOCK_DRIFT_VARIABILITY,
-     'linestyle': 'dashdot',
-     'hatch': 'x'},
-    {'id': 5, 'initial_time_offset': 50, 'clock_drift_rate': random.randint(-CLOCK_DRIFT_RATE_OFFSET_RANGE, CLOCK_DRIFT_RATE_OFFSET_RANGE), 'clock_drift_scale': CLOCK_DRIFT_VARIABILITY,
-     'linestyle': 'dashdot',
-     'hatch': 'x'},
+    {'id': i, 'initial_time_offset': random.randint(0, 500),
+     'clock_drift_rate': random.randint(-CLOCK_DRIFT_RATE_OFFSET_RANGE, CLOCK_DRIFT_RATE_OFFSET_RANGE),
+     'clock_drift_scale': CLOCK_DRIFT_VARIABILITY,
+     'linestyle': 'dashdot'}
+    for i in topo
 ]
-
-########
 
 # Generate graph plot and positions
 edge_list = [(kvs[0], v) for kvs in topo.items() for v in kvs[1]]
 G = nx.from_edgelist(edge_list)
 pos = nx.nx_agraph.graphviz_layout(G)
+
+reception_probabilities = {i['id']: {} for i in init_state}
+for x, i in enumerate(pos):
+    for y, j in enumerate(pos):
+        if i != j:
+            reception_prob = reception_probability(distance_euc(pos[i], pos[j]))
+            reception_probabilities[x][y] = round(reception_prob, 3)
+
+print('reception_probabilities:')
+pp(reception_probabilities)
+
 nx.draw(G, with_labels=True, pos=pos)
-# print(pos)
 
 # Set up data structures for logging
-# node_fires = [[0] * SIM_TIME for _ in init_state]
-# node_suppresses = [[0] * SIM_TIME for _ in init_state]
-# node_phase = [[0] * SIM_TIME for _ in init_state]
-# node_epochs = [[0] * SIM_TIME for _ in init_state]
 node_phase_x = [[] for _ in init_state]
 node_phase_y = [[] for _ in init_state]
+
+node_phase_percentage_x = [[] for _ in init_state]
+node_phase_percentage_y = [[] for _ in init_state]
+
 node_epochs_x = [[] for _ in init_state]
 node_epochs_y = [[] for _ in init_state]
 fire_x = []
@@ -275,8 +311,7 @@ rng = np.random.default_rng(RANDOM_SEED)
 nodes = [PCONode7(env, state) for state in init_state]
 
 for i, node in enumerate(nodes):
-    # node.neighbors = [nodes[n] for n in range(len(nodes)) if n != i]  # [nodes[n] for n in topo[i]]
-    node.neighbors = [nodes[n] for n in topo[i]]
+    node.neighbors = [nodes[n] for n in range(len(nodes)) if n != i]  # [nodes[n] for n in topo[i]]
     node.pos = pos[i]
 
 env.run(until=SIM_TIME)
@@ -309,54 +344,56 @@ ax[1].legend(loc="upper right")
 
 # Node epoch
 for i in init_state:
-    ax[2].plot(node_epochs_x[i['id']], node_epochs_y[i['id']], label='node ' + str(i['id']), linestyle=i['linestyle'], linewidth=2)
+    ax[2].plot(node_epochs_x[i['id']], node_epochs_y[i['id']], label='node ' + str(i['id']), linestyle=i['linestyle'],
+               linewidth=2)
 ax[2].set_title('Node epoch')
 ax[2].set(ylabel='Node epoch')
 ax[2].legend(loc="upper right")
 
 fig.suptitle('Initial time offsets: ' + str([i['initial_time_offset'] for i in init_state]))
 
-
-def phase_diff(a, b):
-    x = abs(a - b)
-    return min(x, DEFAULT_PERIOD_LENGTH - x)
-    # todo: not accurate, because the phase should really be expressed as a % of the current period, not the default
-    #  period
-
 num_broadcasts = len(fire_x)
-range_list_x = []
-range_list_y = []
-stdev_list_x = []
-stdev_list_y = []
-# for i, tick in enumerate(zip(*node_phase_y)):
-# for i, tick in enumerate(zip(node_phase_y[0], node_phase_x[0])):
-#     print(i, tick)
-    # range_list_x.append(i)
-    # range_list_y.append((phase_diff(max(tick), min(tick))) / 2)
-    # stdev_list[i] = statistics.stdev(tick)
-# avg_range = round(sum(range_list_y) / len(range_list_y), 2)
-# avg_stdev = sum(stdev_list) / len(stdev_list)
 
-# Synchronization error
-ax[3].plot(range_list_x, range_list_y, label='range', linewidth=2)
-ax[3].set_title('Synchronization error (max-min)/2 (ticks)')
-ax[3].set(xlabel='Time (ms)', ylabel='Synch error (ticks)')
+
+def calculate_phase_difference(wave1, wave2, max_phase_percentage=100):
+    diff = np.abs(wave1 - wave2)
+    diff_opposite = abs(max_phase_percentage - diff)
+    return np.minimum(diff, diff_opposite)
+
+
+x = np.linspace(0, SIM_TIME, int(SIM_TIME / RECEPTION_LOOP_TICKS))
+
+interpolated = np.array([np.interp(x, node_phase_percentage_x[i], node_phase_percentage_y[i]) for i in
+                         range(len(node_phase_percentage_y))])
+
+differences = []
+# todo: inefficient to do pairwise calcs, change if we're using huge topologies
+# O(n(n-1)/2) complexity :(
+for i in range(len(node_phase_percentage_y)):
+    for j in range(i + 1, len(node_phase_percentage_y)):
+        differences.append(calculate_phase_difference(interpolated[i], interpolated[j]))
+
+differences = np.array(differences)
+mean_differences = np.mean(differences, axis=0)
+
+ax[3].plot(x, mean_differences, label='mean_differences', linewidth=2)
+ax[3].set_title('Average Phase difference')
+ax[3].set(xlabel='Time (ms)', ylabel='Pair-wise avg. phase difference (%)')
+ax[3].legend(loc="upper right")
 
 # Print out metrics for the network
 print('Number of broadcasts:', num_broadcasts)
 # print('Average synch range (ticks):', avg_range)
 # print('Average synch st. dev. (ticks):', avg_stdev)
-# print("Avg. range after synchronization", sum(range_list_y[-DEFAULT_PERIOD_LENGTH:]) / len(range_list_y[-DEFAULT_PERIOD_LENGTH:]))
+print("Avg. phase difference after synchronization", np.mean(mean_differences[-100:]))
 
 # plt.text(-1, -1, "Number of broadcasts: " + str(num_broadcasts))
 plt.text(.80, .90, "Number of broadcasts: " + str(num_broadcasts), ha='left', va='top', transform=ax[3].transAxes)
 # plt.text(.80, .80, "Avg. synch range (ticks): " + str(avg_range), ha='left', va='top', transform=ax[3].transAxes)
 # todo: add topo
 
-# print(range_list_x)
-# print(range_list_y)
-# print(zip(*node_phase_y))
-# print(node_phase_x[0])
-# print(node_phase_y[0])
+# for x in range(7350, 7850):
+#     print(interpolated[0][x], interpolated[1][x], interpolated[2][x], interpolated[3][x], interpolated[4][x], interpolated[5][x], differences[4][x])
+
 
 plt.show()
